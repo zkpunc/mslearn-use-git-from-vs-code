7dab029e9f19c7ebec929b2f34fa8d4d56d41dc3
zkpunc <angelosabokkb62@gmail.com>
2022-12-16 19:21:44 +0800

    revert

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello git merge tes' });
+  res.render('index', { title: 'Hello git revert' });
 });
 
 module.exports = router;
